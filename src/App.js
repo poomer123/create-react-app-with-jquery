99bb1bd7f414277aae26c9cd9755135e9f5d1f77
ApiratChiwakanit <apirat.c@roundtwosolutions.com>
2018-03-21 14:00:06 +0700

    test jquery

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import $ from 'jquery'
 
 class App extends Component {
   render() {
-
+    $('.eiei').addClass('testttt')
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
+        <p className="App-intro eiei">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
